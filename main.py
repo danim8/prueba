2265f49972a2804a113d86a11784eaac19ec1341
Dani <dani.m2@keemail.me>
2020-07-15 17:13:36 +0200

    Arreglado

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 14 19:13:21 2020
+
+@author:Daniel Merelas
+"""
+
+# Problema 1 de proyecto Euler
+# Solucion corta
+# Otro comentario
+
 print(sum(x for x in range(1000) if x%3==0 or x%5==0))
